450203a3dbd52aa20c8dd91c4b7988dd96691c6f
Samuel_Ramos <progsamu@gmail.com>
2023-09-06 12:23:31 -0300

    growflix v2.0

--- assets/script.js
+++ assets/script.js
@@ -1,63 +1,59 @@
 const videos = document.getElementById('videos');
 const painelConteudo = document.getElementById('painelConteudo');
 const modalAqui = document.getElementById('modalAqui');
-const growcast = document.getElementById('growcast');
-const Webinar = document.getElementById('Webinar');
-const ux = document.getElementById('ux');
-const diversos = document.getElementById('diversos');
 
-function getVideo() {
-  for (const item of catalogo) {
-    const img = item.img;
-    const titulo = item.titulo;
-    const link = item.link;
-    const categoria = item.categoria;
+const filterGrowcast = catalogo.filter((item) => item.categoria === 'growcast');
+const filterWebinar = catalogo.filter((item) => item.categoria === 'Webinar');
+const filterUX = catalogo.filter((item) => item.categoria === 'UX/UI');
+const filterDiversos = catalogo.filter((item) => item.categoria === 'Diversos');
 
-    painelConteudo.innerHTML += `
-      <div class="col-3 iframe-container abrir-modal conteudo cardVideos p-3">
-        <div class="row">
-          <img class="img w-100" onclick="verVideo({ titulo: '${titulo}', link: '${link}' })" data-bs-toggle="modal" data-bs-target="#myModal" src="${img}" alt="${titulo}">
-        </div>
-        <div class="row text-light text-center fs-4 complemento-img m-auto textoVideo">
-          <div class="col-2 py-2 fs-1" >
-            <i class="bi bi-play-circle"></i>
-          </div>
-          <div class="col-10">
-            <div class="h-100 d-flex align-items-center justify-content-start">
-              <p class="p-0 mx-3 my-2 tituloVideo">${item.titulo}</p>
-            </div>
+function criarVideoHTML(item) {
+  const img = item.img;
+  const titulo = item.titulo;
+  const link = item.link;
+  
+  return `
+    <div class="col-12 col-md-6 col-xl-4 iframe-container abrir-modal conteudo cardVideos p-3">
+      <div class="row">
+        <img class="img w-100" src="${img}" alt="${titulo}" style="height:200px;">
+        <div class="playerCard m-auto col-12"> <i class="bi bi-play-fill" onclick="verVideo('${titulo}', '${link}')"  data-bs-toggle="modal" data-bs-target="#myModal" ></i> </div>
+      </div>
+      <div class="row text-light text-center fs-4 complemento-img m-auto textoVideo">
+        <div class="col-12">
+          <div class="h-100 d-flex align-items-center justify-content-start">
+            <p class="p-0 mx-1 my-2 tituloVideo">${titulo}</p>
           </div>
         </div>
       </div>
-    `;
-  }
-}
-// const videosFiltrados = catalogo.filter(video => video.categoria === categoriaSelecionada);
-
-getVideo();
-
-function verVideo(videoInfo) {
-  const titulo = videoInfo.titulo;
-  const link = videoInfo.link;
-  modalAqui.innerHTML = `
-<div class="modal fade" id="myModal" tabindex="-1">
-  <div class="modal-dialog modal-fullscreen">
-    <div class="modal-content bg-black">
-      <div class="modal-header text-light">
-        <h5 class="modal-title ">Assista ao vídeo</h5>
-        <button type="button" class="btn-close" onclick="pauseVideo()" data-bs-dismiss="modal" aria-label="Close"> <i class="bi bi-x-circle"></i> </button>
-      </div>
-      
-        <div class="embed-responsive embed-responsive-16by9">
-        <iframe id="videoFrame" class="embed-responsive-item m-auto" src="${link}" title="YouTube video player" 
-        frameborder="0" style="width: 100vw; height:90vh;" allow="accelerometer; autoplay; clipboard-write; encrypted-media; 
-        gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-        </div>
-      
     </div>
-  </div>
-</div>
-`
+  `;
 }
 
+function renderizarVideosComNomeSessao(videos, container, nomeSessao) {
+  const htmlNomeSessao = `<h2>${nomeSessao}</h2>`;
+  const htmlVideos = videos.map(criarVideoHTML).join('');
+  container.innerHTML = htmlNomeSessao + htmlVideos;
+}
 
+renderizarVideosComNomeSessao(filterGrowcast, growcast, "Growcast");
+renderizarVideosComNomeSessao(filterWebinar, Webinar, "Webinar em Flutter");
+renderizarVideosComNomeSessao(filterUX, ux, "Jornada UX/UI");
+renderizarVideosComNomeSessao(filterDiversos, diversos, "Diversos");
+
+function verVideo(titulo, link) {
+  modalAqui.innerHTML = `
+    <div class="modal fade" id="myModal" tabindex="-1">
+      <div class="modal-dialog modal-fullscreen">
+        <div class="modal-content bg-black">
+          <div class="modal-header text-light">
+            <h5 class="modal-title ">Assista ao vídeo</h5>
+            <button type="button" class="btn-close" onclick="pauseVideo()" data-bs-dismiss="modal" aria-label="Close"> <i class="bi bi-x-circle"></i> </button>
+          </div>
+          <div class="embed-responsive embed-responsive-16by9">
+            <iframe id="videoFrame" class="embed-responsive-item m-auto" src="${link}" title="YouTube video player" frameborder="0" style="width: 100vw; height:90vh;" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+          </div>
+        </div>
+      </div>
+    </div>
+  `;
+}
